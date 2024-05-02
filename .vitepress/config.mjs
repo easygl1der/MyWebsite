0444d9d1261214738c36eecacf5ecacc41fdf13f
easyglider <easygl1der@outlook.com>
2024-05-03 01:21:41 +0800

    添加 生涯、随手记录

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -11,7 +11,9 @@ export default defineConfig({
       { text: 'Home', link: '/' },
       { text: '联系方式', link: '/联系方式' },
       { text: '作业', link: '/作业'},
-      { text: '公众号', link: '/公众号'}
+      { text: '公众号', link: '/公众号'},
+      { text: '数学生涯', link: '/数学生涯'},
+      { text: '随手记录', link: '/随手记录'}
     ],
     footer: {
       copyright: "Copyright@2024 Yue Yihua",
